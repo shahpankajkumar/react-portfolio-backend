cecce89616307fcc88534f1a8d7fad5d6fbed066
Pankaj S Shah <pankaj.s.shah@doyenhub.com>
2024-09-20 12:04:32 +0530

    Adding a vercel file.

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ connectDB();
 app.use(express.json());
 
 app.use('/api/email', require('./routes/email'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
